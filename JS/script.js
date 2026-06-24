13f790b2b61bdfe3cc6ac6577ea4b3d793024826
HaFran002 <airbrushed002@gmail.com>
2026-06-24 00:05:35 -0300

    melhorias

--- JS/script.js
+++ JS/script.js
@@ -1,37 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
 
+  const menuLinks = document.querySelectorAll('#menu a');
 
-const mobileBtn = document.getElementById('menu-mobile');
-const menu = document.getElementById('menu');
-
-mobileBtn.addEventListener('click', () => {
-  menu.classList.toggle('active');
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('active');
+  });
 });
 
+  const mobileBtn = document.getElementById('menu-mobile');
+  const menu = document.getElementById('menu');
 
+  mobileBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+  });
 
-const form = document.getElementById('reviewForm');
-const wrapper = document.getElementById('testimonial-wrapper');
+  const form = document.getElementById('reviewForm');
+  const wrapper = document.getElementById('testimonial-wrapper');
 
-form.addEventListener('submit', function(e){
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
 
-  e.preventDefault();
+    const name = document.getElementById('name').value;
+    const message = document.getElementById('message').value;
 
-  const name = document.getElementById('name').value;
-  const message = document.getElementById('message').value;
+    const card = document.createElement('div');
+    card.classList.add('testimonial');
 
-  const card = document.createElement('div');
+    card.innerHTML = `
+      <p>${message}</p>
+      <div class="testimonial-user">${name}</div>
+    `;
 
-  card.classList.add('testimonial');
-
-  card.innerHTML = `
-    <p>${message}</p>
-    <div class="testimonial-user">${name}</div>
-  `;
-
-  wrapper.prepend(card);
-
-  form.reset();
-
-  alert('Depoimento enviado com sucesso!');
+    wrapper.prepend(card);
+    form.reset();
+    card.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  });
 
 });
